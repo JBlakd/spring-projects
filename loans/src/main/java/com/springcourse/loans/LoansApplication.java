48606dc804ac8801da2671ebc0edc5b584d16bc9
Ivan Hu <ivanhu2246@gmail.com>
2022-08-07 18:18:51 +0800

    Recreated loans microservice

---
+++ loans/src/main/java/com/springcourse/loans/LoansApplication.java
@@ -0,0 +1,20 @@
+package com.springcourse.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScans({ @ComponentScan("com.springcourse.loans.controller") })
+@EnableJpaRepositories("com.springcourse.loans.repository")
+@EntityScan("com.springcourse.loans.model")
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
---
+++ loans/src/main/java/com/springcourse/loans/controller/LoansController.java
@@ -0,0 +1,36 @@
+package com.springcourse.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcourse.loans.model.Customer;
+import com.springcourse.loans.model.Loans;
+import com.springcourse.loans.repository.LoansRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class LoansController {
+
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+        List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+
+    }
+
+}
