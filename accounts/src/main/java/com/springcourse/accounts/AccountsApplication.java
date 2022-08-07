00332bb65210d89374342e9bc831e468fbc24e00
Ivan Hu <ivanhu2246@gmail.com>
2022-08-07 17:41:56 +0800

    Recreated accounts microservice

---
+++ accounts/src/main/java/com/springcourse/accounts/AccountsApplication.java
@@ -0,0 +1,20 @@
+package com.springcourse.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScans({@ComponentScan("com.springcourse.accounts.controller")})
+@EnableJpaRepositories("com.springcourse.accounts.repository")
+@EntityScan("com.springcourse.accounts.model")
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
