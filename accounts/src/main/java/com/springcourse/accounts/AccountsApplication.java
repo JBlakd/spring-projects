bcfe762d24e234f3e5d3165f3eca7f0149684240
Ivan Hu <ivanhu2246@gmail.com>
2022-08-12 17:47:36 +0800

    Added support for feign clients

--- accounts/src/main/java/com/springcourse/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/springcourse/accounts/AccountsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ComponentScans({@ComponentScan("com.springcourse.accounts.controller")})
 @EnableJpaRepositories("com.springcourse.accounts.repository")
+@EnableFeignClients
 @EntityScan("com.springcourse.accounts.model")
 @RefreshScope
 public class AccountsApplication {
--- accounts/src/main/java/com/springcourse/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/springcourse/accounts/controller/AccountsController.java
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.springcourse.accounts.config.AccountsServiceConfig;
-import com.springcourse.accounts.model.Properties;
+import com.springcourse.accounts.model.*;
+import com.springcourse.accounts.service.client.CardsFeignClient;
+import com.springcourse.accounts.service.client.LoansFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springcourse.accounts.model.Accounts;
-import com.springcourse.accounts.model.Customer;
 import com.springcourse.accounts.repository.AccountsRepository;
+
+import java.util.List;
 
 @RestController
 public class AccountsController {
@@ -43,6 +45,27 @@ public class AccountsController {
                 accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
         String jsonStr = ow.writeValueAsString(properties);
         return jsonStr;
+    }
+
+    @Autowired
+    LoansFeignClient loansFeignClient;
+
+    @Autowired
+    CardsFeignClient cardsFeignClient;
+
+    @PostMapping("/myCustomerDetails")
+    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+
+        return customerDetails;
+
     }
 
 }
